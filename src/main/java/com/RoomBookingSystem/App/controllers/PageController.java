eedd0e01cc1dbb075d35157f32971b076d6b9cc3
MattToczek <codenation@Codes-MacBook-Pro.local>
2020-01-13 17:55:04 +0000

    adding dynamic element to front end

--- src/main/java/com/RoomBookingSystem/App/controllers/PageController.java
+++ src/main/java/com/RoomBookingSystem/App/controllers/PageController.java
@@ -3,14 +3,22 @@ package com.RoomBookingSystem.App.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
+    private String name = "Matthew!!!";
 
-    @GetMapping("/home")
-    public String giveOptions(){
+
+    @GetMapping("/")
+    public String giveOptions(Model model){
+
+        // Passes string (name) in, where defined in home.html
+        model.addAttribute("user", name);
+
         return "home";
+
     }
 
 }
