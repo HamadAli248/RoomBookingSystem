9a0344940ca1c0e09b8d5057b087fde82735c929
hammadali248 <hammadali248@gamil.com>
2020-01-14 11:31:26 +0000

    added postgresql connection

--- src/main/java/com/RoomBookingSystem/App/AppApplication.java
+++ src/main/java/com/RoomBookingSystem/App/AppApplication.java
@@ -3,13 +3,16 @@ package com.RoomBookingSystem.App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
 
 
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(AppApplication.class, args);
+		Data arr = new Data();
+
 
 	}
 
