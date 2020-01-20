6832712d019ed567edce5c91873028f3371edf06
MattToczek <codenation@Codes-MacBook-Pro.local>
2020-01-20 12:09:05 +0000

    all functionality working with both databases - only clash checker to add

--- src/main/java/com/RoomBookingSystem/App/MySqlDataBaseQuery.java
+++ src/main/java/com/RoomBookingSystem/App/MySqlDataBaseQuery.java
@@ -40,6 +40,8 @@ public class MySqlDataBaseQuery {
 
                 }
 
+
+
                 connect.close();
 
             } catch (ClassNotFoundException | SQLException e) {
@@ -49,6 +51,41 @@ public class MySqlDataBaseQuery {
 
 
         }
-}
+        public MySqlDataBaseQuery(SimpleBooking currBook) throws Exception {
+
+            Connection connect = null;
+
+            try {
+                // directs to driver (added in the dependencies)
+                Class.forName("com.mysql.jdbc.Driver");
+                connect = DriverManager
+                        //info from workbench - port/schemaName, user, password
+                        .getConnection("jdbc:mysql://localhost:3306/JavaProjectDatabase",
+                                "root", "password");
+
+                System.out.println("DB Opened Successfully");
+
+                String query = " insert into bookings(start_time,start_date,end_time,end_date,booker_name,room_number)" +
+                        "values(?,?,?,?,?,?)";
+                PreparedStatement preparedStatement = connect.prepareStatement(query);
+                preparedStatement.setTime(1, new java.sql.Time(currBook.getStartTime().getTime()));
+                preparedStatement.setDate(2,new java.sql.Date(currBook.getStartDate().getTime()));
+                preparedStatement.setTime(3,new java.sql.Time(currBook.getEndTime().getTime()));
+                preparedStatement.setDate(4, new java.sql.Date(currBook.getEndDate().getTime()));
+                preparedStatement.setString(5,currBook.getName());
+                preparedStatement.setInt(6,currBook.getRoom());
+                preparedStatement.execute();
+                System.out.println("==================================================Your Booking has been entered in the database========================================");
+
+                connect.close();
+
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+
+
+            }
+        }
+
+    }
 
 
